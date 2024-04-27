319aa4ce500a82f99cd2ca46340e5b6915589f33
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-04-27 12:34:18 -0700

    create timer component and method that gets the next friday from today's date
    Co-authored-by: Jackie Ou <jackie-ou@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Timer from "./Timer";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // target time
+  const nextFriday = () => {
+    const today = new Date();
+    const friday = new Date(today.getTime());
+    friday.setDate(today.getDate() + ((7 + 5 - today.getDay()) % 7));
+    friday.setHours(10);
+    friday.setMinutes(0);
+    friday.setSeconds(0);
+    return friday;
+  };
+  let meetingTime = nextFriday();
+  // decrement time
+  // countdown to time in seconds
+  // parse into how many days, hours, etc
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>TIME UNTIL NEXT MANAGER's MEETING</h1>
+      <h2>{meetingTime.toString()}</h2>
+      <div className="timer-container">
+        <Timer number={13} unit={"Days"} />
+        <Timer number={23} unit={"Hours"} />
+        <Timer number={59} unit={"Minutes"} />
+        <Timer number={58} unit={"Seconds"} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Timer.jsx
@@ -0,0 +1,10 @@
+const Timer = ({ number, unit }) => {
+  return (
+    <div>
+      <p>{number}</p>
+      <p>{unit}</p>
+    </div>
+  );
+};
+
+export default Timer;
